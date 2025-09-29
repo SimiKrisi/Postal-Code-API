04ea45664bee05ac74320c1953e947199807120e
Global User <nagy.zsolt2005@tanulo.boronkay.hu>
2025-09-29 10:03:38 +0200

    County hozzáadása

--- database/migrations/2025_09_29_072023_create_country_table.php
+++ database/migrations/2025_09_29_072023_create_country_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('country', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->autoIncrement();
-            
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('country');
+        Schema::dropIfExists('countries');
     }
 };
