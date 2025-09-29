99c2836899b5518c4d21f0f936839f7fc2c3a5e5
Global User <nagy.zsolt2005@tanulo.boronkay.hu>
2025-09-29 11:04:18 +0200

    fix pont3

--- app/Models/PostalCode.php
+++ app/Models/PostalCode.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class PostalCode extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['code']['name']['countyid'];
+    protected $fillable = ['code','name','countyid'];
 }
